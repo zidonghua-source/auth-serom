557a4e830c7f783e4b4a2b39cc5deba3edf9f3d3
11111111 <127168437+khongluadao@users.noreply.github.com>
2026-05-02 23:54:34 +0700

    B

--- routes/base_routes.py
+++ routes/base_routes.py
@@ -1,37 +1,8 @@
-import os
-from concurrent.futures import ThreadPoolExecutor, TimeoutError as FutureTimeoutError
-from functools import wraps
-from flask import Blueprint, copy_current_request_context, jsonify
+from flask import Blueprint
 from controllers.base_controller import index, healthy, devices, login, logout, work, log_api
 from extensions import db
 
 base_bp = Blueprint('base', __name__)
-REQUEST_TIMEOUT_SECONDS = int(os.environ.get("REQUEST_TIMEOUT_SECONDS", "8"))
-ROUTE_TIMEOUT_WORKERS = int(os.environ.get("ROUTE_TIMEOUT_WORKERS", "20"))
-_route_timeout_executor = ThreadPoolExecutor(max_workers=ROUTE_TIMEOUT_WORKERS)
-
-
-def route_timeout(seconds: int):
-    def decorator(view_func):
-        @wraps(view_func)
-        def wrapper(*args, **kwargs):
-            @copy_current_request_context
-            def run_view():
-                return view_func(*args, **kwargs)
-
-            future = _route_timeout_executor.submit(run_view)
-            try:
-                return future.result(timeout=seconds)
-            except FutureTimeoutError:
-                return jsonify({"status": "timeout", "message": "Request timeout"}), 504
-
-        return wrapper
-
-    return decorator
-
-
-def _with_timeout(view_func):
-    return route_timeout(REQUEST_TIMEOUT_SECONDS)(view_func)
 
 
 @base_bp.after_request
@@ -45,10 +16,10 @@ def release_db_session(_error):
     db.session.remove()
 
 
-base_bp.route('/', methods=['GET'])(_with_timeout(index))
-base_bp.route('/healthy', methods=['GET', 'POST'])(_with_timeout(healthy))
-base_bp.route('/devices', methods=['GET'])(_with_timeout(devices))
-base_bp.route('/login', methods=['GET', 'POST'])(_with_timeout(login))
-base_bp.route('/logout', methods=['GET'])(_with_timeout(logout))
-base_bp.route('/work', methods=['GET', 'POST'])(_with_timeout(work))
-base_bp.route('/log', methods=['GET', 'POST'])(_with_timeout(log_api))
+base_bp.route('/', methods=['GET'])(index)
+base_bp.route('/healthy', methods=['GET', 'POST'])(healthy)
+base_bp.route('/devices', methods=['GET'])(devices)
+base_bp.route('/login', methods=['GET', 'POST'])(login)
+base_bp.route('/logout', methods=['GET'])(logout)
+base_bp.route('/work', methods=['GET', 'POST'])(work)
+base_bp.route('/log', methods=['GET', 'POST'])(log_api)
